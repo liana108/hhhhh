b622897237e60a5df2491d51995a21edf5f5b388
Liana Veldiaeva <lianali108@gmail.com>
2020-07-08 01:43:23 +0200

    fixed #1

---
+++ newbonus/bonus-service/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+        // выводим результат
+        System.out.println(passed);
+    }
+
+}
+
+
+
